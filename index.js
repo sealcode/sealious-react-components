3156db0f802afdb39e327aae38f60db526654182
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-08-04 14:00:05 +0200

    Connect select component with higher order component. Add ES6 features

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import Test from './Test.jsx';
 import ResourceList from './lib/resource-list.jsx';
 import ResourceTable from './lib/resource-table.jsx';
 import ResourceForm from './lib/resource-create-form.jsx';
-
+import ResourceSelect from './lib/resource-select.jsx';
 //higher order
 
 import ResourceListES6 from './lib/resource-list-es6.jsx';
 import ResourceTableES6 from './lib/resource-table-es6.jsx';
-
+import ResourceSelectES6 from './lib/resource-select-es6.jsx';
 //lista
 /*ReactDOM.render(
   <ResourceList
@@ -45,14 +45,14 @@ import ResourceTableES6 from './lib/resource-table-es6.jsx';
 );*/
 
 //higher order table
-ReactDOM.render(
+/*ReactDOM.render(
     <ResourceTableES6
       url='http://sealcode.org:8082/api/v1/resources/task'
       paginate={true}
-      itemsPerPage={3}
+      itemsPerPage={6}
     />,
     document.getElementById('app')
-  );
+  );*/
 //higher order form
 /*let myModel = {
   name: "text",
@@ -69,3 +69,11 @@ ReactDOM.render(
 ReactDOM.render(
   <ResourceForm model={myModel}/>,
   document.getElementById('app'));*/
+  ReactDOM.render(
+    <ResourceSelectES6
+      url='http://sealcode.org:8082/api/v1/resources/task'
+      displayAttr="title"
+
+    />,
+    document.getElementById('app')
+  );
---
+++ lib/resource-select-es6.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ResourceTypeCollection from './mixins/resource-type-collection-es6.jsx';
+import merge from "merge";
+
+const ResourceSelect = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	        displayAttr: "id",
+	        noValueOptionName: "--",
+	        allowNoValue: false
+	    };
+	},
+	getValue: function(){
+		console.log("getValue!", this.refs.select.value);
+		if(this.refs.select.value == "undefined"){
+			return undefined;
+		}else{
+			return this.refs.select.value;
+		}
+	},
+	handleChange: function(e){
+		this.props.onChange && this.props.onChange(e)
+	},
+	render: function(){
+		var options = this.props.resources.map((resource) => {
+			return (
+				<option value={this.props.valueAttr? resource.body[this.props.valueAttr] : resource.id} key={resource.id}>
+					{resource[this.props.displayAttr] || resource.body[this.props.displayAttr]}
+				</option>)
+		})
+
+		if(this.props.allowNoValue){
+			options.unshift(
+				<option value="undefined" key="empty">
+					{this.props.noValueOptionName}
+				</option>
+			)
+		}
+
+		console.log("this.props.disabled", this.props.disabled);
+		return (
+			<label>
+				{this.props.label}
+				<select ref="select" onChange={this.handleChange} value={this.props.value} disabled={this.props.disabled || false}>
+					{options}
+				</select>
+			</label>
+		)
+	}
+});
+
+export default ResourceTypeCollection(ResourceSelect);
