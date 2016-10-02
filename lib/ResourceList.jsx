7ec4896dd876ca42f430663c8134419c5f3228d5
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-10-02 20:06:59 +0200

    Split resource table into pure table function and higher order function

--- lib/ResourceList.jsx
+++ lib/ResourceList.jsx
@@ -1,5 +1,4 @@
 import resourceTypeCollection from './mixins/resourceTypeCollection.jsx';
-
 import PureResourceList from './ResourceListPure.jsx';
 
 export default resourceTypeCollection(PureResourceList);
