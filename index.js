b4e74838e5fac5b266174dc23807abee50cf33d9
NekoBard <tadeusz.m.lewandowski@gmail.com>
2016-10-03 20:07:15 +0200

    Prepare index.js to export

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Test from './Test.jsx';
-import TestSingle from './TestSingle.jsx';
-//higher order
-import ResourceList from './lib/resource-list.jsx';
-import ResourceTable from './lib/resource-table.jsx';
-import ResourceSelect from './lib/resource-select.jsx';
-import SingleResource from './lib/mixins/single-resource.jsx';
-//lista
-// ReactDOM.render(
-//   <ResourceList
-//     url='http://sealcode.org:8082/api/v1/resources/task'
-//     listElementClass={Test}
-//     paginate={true}
-//     itemsPerPage={2}
-//   />,
-//   document.getElementById('app')
-// );
+import ResourceList from './lib/ResourceList.jsx';
+import ResourceTable from './lib/ResourceTable.jsx';
+import ResourceSelect from './lib/ResourceSelect.jsx';
+import singleResource from './lib/mixins/singleResource.jsx';
 
-
-
-const Single = SingleResource(TestSingle);
-
-ReactDOM.render(
-  <Single url='http://sealcode.org:8082/api/v1/resources/task/jch9vlefet'/>,
-  document.getElementById('app')
-);
-
-
-//tabela
-/*ReactDOM.render(
-  <ResourceTable url='http://sealcode.org:8082/api/v1/resources/task'
-    paginate={true}
-    itemsPerPage={3}
-
-  />,
-  document.getElementById('app'));*/
-
-//select
-/*ReactDOM.render(
-  <ResourceSelect
-    url='http://sealcode.org:8082/api/v1/resources/task'
-    displayAttr="title"
-
-  />,
-  document.getElementById('app')
-);*/
+module.export = {
+  ResourceList: ResourceList,
+  ResourceTable: ResourceTable,
+  ResourceSelect: ResourceSelect,
+  singleResource: singleResource,
+}
