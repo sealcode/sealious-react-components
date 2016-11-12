295c22d389774ea855c54478e1f4e0b5a4cacf59
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-12 13:14:51 +0100

    Resolve issues with dangling event handlers

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ module.exports = {
   loadPageNumberFromLocation: require("./lib/mixins/load-page-number-from-location.js"),
   keepPageNumberInState: require("./lib/mixins/keep-page-number-in-state.js"),
   RouterLink: require("./lib/RouterLink.js"),
-  propsFromUrl: require("./lib/mixins/propsFromUrl.js"), 
+  propsFromUrl: require("./lib/mixins/props-from-url.js"), 
 };
