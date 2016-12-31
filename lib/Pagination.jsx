2a4f58cfa289affef0d6e6d562f1a12d7666c571
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-12-31 13:39:05 +0100

    Change names for buttons in pagination

--- lib/Pagination.jsx
+++ lib/Pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = function(props){
 					style={{visibility: props.hasPrev? "visible" : "hidden"}}
 				>
 					<a href={props.prevPageUrl} onClick={props.onPrev} key="navigation-prev-anchor">
-						&#x25C0; poprzednie
+						Poprzednia&nbsp;strona
 					</a>
 				</li>
 
@@ -20,7 +20,7 @@ const Pagination = function(props){
 					style={{visibility: props.hasNext? "visible" : "hidden"}}
 				>
 					<a href={props.nextPageUrl} onClick={props.onNext} key="navigation-next-anchor">
-						następne &#x25B6;
+						Następna&nbsp;strona
 					</a>
 				</li>
 			</ul>
